3033ad70265fa3d5df48e90207d4eaf01430089e
AudeizReading <aaericia@hotmail.fr>
2021-03-10 16:12:41 +0100

    Cleaning of my github account. Rearrange in only one (or two) repository my work into PISCINE 42 Nice

---
+++ C08/main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alellouc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/24 10:08:14 by alellouc          #+#    #+#             */
+/*   Updated: 2021/03/02 20:57:22 by alellouc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+void	ft_putstr(char *str);
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+int main(void)
+{
+	printf();
+	return (0);
+}
